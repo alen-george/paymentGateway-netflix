13305bb786a2484242515788c4a9c2be2f93c3d5
Alen George <al3ngeorge@gmail.com>
2022-05-20 18:26:39 +0530

    paymentgateway-integration

---
+++ paymentRoute.js
@@ -0,0 +1,129 @@
+require("dotenv").config();
+
+const formidable = require("formidable");
+const express = require("express");
+const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+const https = require("https");
+const PaytmChecksum = require("./PaytmChecksum");
+
+
+const netflixBrowsePage = 'http://localhost:3000'
+const backEndCallback =' http://localhost:5000'
+
+
+router.post("/callback", (req, res) => {
+    const form = new formidable.IncomingForm();
+  
+    form.parse(req, (err, fields, file) => {
+      let paytmChecksum = fields.CHECKSUMHASH;
+      delete fields.CHECKSUMHASH;
+  
+      var isVerifySignature = PaytmChecksum.verifySignature(
+        fields,
+        process.env.PAYTM_MERCHANT_KEY,
+        paytmChecksum
+      );
+      if (isVerifySignature) {
+        var paytmParams = {};
+        paytmParams["MID"] = fields.MID;
+        paytmParams["ORDERID"] = fields.ORDERID;
+  
+        /*
+         * Generate checksum by parameters we have
+         * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys
+         */
+        PaytmChecksum.generateSignature(
+          paytmParams,
+          process.env.PAYTM_MERCHANT_KEY
+        ).then(function (checksum) {
+          paytmParams["CHECKSUMHASH"] = checksum;
+  
+          var post_data = JSON.stringify(paytmParams);
+  
+          var options = {
+            /* for Staging */
+            hostname: "securegw-stage.paytm.in",
+  
+            /* for Production */
+            // hostname: 'securegw.paytm.in',
+  
+            port: 443,
+            path: "/order/status",
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Content-Length": post_data.length,
+            },
+          };
+  
+          var response = "";
+          var post_req = https.request(options, function (post_res) {
+            post_res.on("data", function (chunk) {
+              response += chunk;
+            });
+  
+            post_res.on("end", function () {
+              let result = JSON.parse(response);
+              if (result.STATUS === "TXN_SUCCESS") {
+
+                res.redirect(`${netflixBrowsePage}/browse`)
+                // res.json({status:`Transaction successful`});
+              }
+  
+              
+            });
+          });
+  
+          post_req.write(post_data);
+          post_req.end();
+        });
+      } else {
+        res.json({status:'Transaction Failed'})
+      }
+    });
+  }); 
+
+router.post("/payment", (req, res) => {
+  const { amount, email } = req.body;
+  console.log(req.body);
+
+  /* import checksum generation utility */
+  const totalAmount = JSON.stringify(amount);
+  const mail = JSON.stringify(email)
+  var params = {};
+
+  /* initialize an array */
+  (params["MID"] = process.env.PAYTM_MID),
+    (params["WEBSITE"] = process.env.PAYTM_WEBSITE),
+    (params["CHANNEL_ID"] = process.env.PAYTM_CHANNEL_ID),
+    (params["INDUSTRY_TYPE_ID"] = process.env.PAYTM_INDUSTRY_TYPE_ID),
+    (params["ORDER_ID"] = uuidv4()),
+    (params["CUST_ID"] = process.env.PAYTM_CUST_ID),
+    (params["TXN_AMOUNT"] = totalAmount),
+    (params["CALLBACK_URL"] = `${backEndCallback}/api/callback`),
+    (params["EMAIL"] = mail),
+    (params["MOBILE_NO"] = mail);
+
+  /**
+   * Generate checksum by parameters we have
+   * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys
+   */
+  var paytmChecksum = PaytmChecksum.generateSignature(
+    params,
+    process.env.PAYTM_MERCHANT_KEY
+  );
+  paytmChecksum
+    .then(function (checksum) {
+      let paytmParams = {
+        ...params,
+        CHECKSUMHASH: checksum,
+      };
+      res.json(paytmParams);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+module.exports = router;
